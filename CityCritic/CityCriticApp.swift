f13fdb95cab5e37641363d2157ae84975f8ba1ee
Pedro <prthomas808@gmail.com>
2024-01-17 08:18:23 -0500

    Initial Commit

---
+++ CityCritic/CityCriticApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityCriticApp.swift
+//  CityCritic
+//
+//  Created by Pedro Thomas on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CityCriticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
